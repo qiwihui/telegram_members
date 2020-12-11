cab6b0c560a459fb444caf735a71d26894df8460
qiwihui <qwh005007@gmail.com>
2020-12-11 13:37:32 +0800

    fix: add config example

---
+++ config.example.py
@@ -0,0 +1,5 @@
+TELEGRAM_ACCOUNTS = [
+    {"phone": "+11234567890", "app_id": 123456, "app_hash": "a1b2c3d4e5f6ghijklmnopqrstuvwxyz"},
+]
+MESSAGES_TEMPLATE = ["Hi {}, nice to meet you."]
+SLEEP_TIME = 30
